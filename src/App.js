86bd709464c1294c6a9977bf3108b03894a1333a
MSalman6 <salman666online@gmail.com>
2021-11-18 22:43:41 +0500

    converted html to react

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import DaoLight from './DaoLight';
+import DaoDark from './DaoDark';
+import FarmingLight from './FarmingLight';
+import FarmingDark from './FarmingDark';
+import LockingLight from './LockingLight';
+import LockingDark from './LockingDark';
+import NewLockLight from './NewLockLight';
+import NewLockDark from './NewLockDark';
+import StakingLight from './StakingLight';
+import StakingDark from './StakingDark';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/dao-light">
+          <DaoLight />
+        </Route>
+        <Route exact path="/dao-dark">
+          <DaoDark />
+        </Route>
+        <Route exact path="/farming-light">
+          <FarmingLight />
+        </Route>
+        <Route exact path="/farming-dark">
+          <FarmingDark />
+        </Route>
+        <Route exact path="/locking-light">
+          <LockingLight />
+        </Route>
+        <Route exact path="/locking-dark">
+          <LockingDark />
+        </Route>
+        <Route exact path="/new-lock-light">
+          <NewLockLight />
+        </Route>
+        <Route exact path="/new-lock-dark">
+          <NewLockDark />
+        </Route>
+        <Route exact path="/staking-light">
+          <StakingLight />
+        </Route>
+        <Route exact path="/staking-dark">
+          <StakingDark />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
